0355d62089024966eeeed7fd2bf109bbcfaa66e0
Alexandru Csete <oz9aec@gmail.com>
2013-06-15 22:03:58 +0200

    Autostart audio stream.

--- systemd/autostart-video.sh
+++ systemd/autostart-video.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-/home/root/bin/cam init > /dev/null 2>&1
-exec /home/root/bin/video-server 192.168.254.141 4000 > /dev/null 2>&1
 
+# initialize camera ho H.264
+/home/root/bin/cam init > /dev/null 2>&1
+
+# start audio
+/home/root/bin/audio-server 192.168.1.100 4002 > /dev/null 2>&1 &
+
+# start video
+exec /home/root/bin/video-server 192.168.1.100 4000 > /dev/null 2>&1
