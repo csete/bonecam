49db9e2fe3c4896a82498806be894956aef3c3ff
Alexandru Csete <oz9aec@gmail.com>
2014-05-07 00:44:14 +0200

    host: Update audio port number.

--- host/audio_client_raw.sh
+++ host/audio_client_raw.sh
@@ -2,5 +2,5 @@
 # Host side audio receiver for raw TCP stream.
 #
 
-gst-launch-1.0 -e -v tcpserversrc host=0.0.0.0 port=4100 ! audio/x-raw,format=\(string\)S16LE,rate=48000,channels=1 ! autoaudiosink
+gst-launch-1.0 -e -v tcpserversrc host=0.0.0.0 port=9100 ! audio/x-raw,format=\(string\)S16LE,rate=48000,channels=1 ! autoaudiosink
 
