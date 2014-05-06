5cddeb5f50abe30e30914485777c2c5dfc59d9da
Alexandru Csete <oz9aec@gmail.com>
2014-05-06 21:05:46 +0200

    host: Update video port.

--- host/video_client.sh
+++ host/video_client.sh
@@ -1,1 +1,1 @@
-gst-launch-1.0 -v udpsrc port=4000 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! avdec_h264 ! fpsdisplaysink fps-update-interval=5000 sync=false
+gst-launch-1.0 -v udpsrc port=9040 caps='application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)H264' ! rtph264depay ! avdec_h264 ! fpsdisplaysink fps-update-interval=5000 sync=false
